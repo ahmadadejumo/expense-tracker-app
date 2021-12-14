b71bd15f806a1a7897e867a862d688a516fdf760
ahmadadejumo <adejumoahmad@gmail.com>
2021-12-14 15:00:00 +0100

    initializing git

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter App"),
+      ),
+      body: Center(
+        child: Text("Widget Playground"),
+      ),
+    );
+  }
+}
