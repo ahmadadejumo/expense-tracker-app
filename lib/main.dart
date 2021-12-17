948da0550fbbb38fed721b38961f2bcea21cf71c
ahmadadejumo <adejumoahmad@gmail.com>
2021-12-17 03:27:11 +0100

    added images to the app

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,23 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'My Expenses',
-      theme: ThemeData(primaryColor: Colors.blueAccent),
+      theme: ThemeData(
+        primaryColor: Colors.blueAccent,
+        accentColor: Colors.lightBlue,
+        fontFamily: "Quicksand",
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: TextStyle(
+                fontFamily: "OpenSans",
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+        appBarTheme: AppBarTheme(
+          textTheme: ThemeData.light().textTheme.copyWith(
+                bodyText1: TextStyle(fontFamily: "OpenSans", fontSize: 20),
+              ),
+        ),
+      ),
       home: MyHomePage(),
     );
   }
@@ -28,18 +44,18 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   // const MyHomePage({Key? key}) : super(key: key);
   final List<Transaction> _userTransactions = [
-    Transaction(
-      id: "t1",
-      title: "New Books",
-      amount: 100,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: "t2",
-      title: "Transport Fare",
-      amount: 65.99,
-      date: DateTime.now(),
-    ),
+    // Transaction(
+    //   id: "t1",
+    //   title: "New Books",
+    //   amount: 100,
+    //   date: DateTime.now(),
+    // ),
+    // Transaction(
+    //   id: "t2",
+    //   title: "Transport Fare",
+    //   amount: 65.99,
+    //   date: DateTime.now(),
+    // ),
   ];
 
   void _addNewTransactions(String txTitle, double txAmount) {
@@ -71,7 +87,10 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("My Expenses"),
+        title: Text(
+          "My Expenses",
+          style: TextStyle(fontFamily: "Open Sans"),
+        ),
         actions: [
           IconButton(
             icon: Icon(Icons.add),
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -12,43 +12,62 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 500,
-      child: ListView.builder(
-        itemBuilder: (ctx, index) {
-          return Card(
-            child: Row(
+      child: transactions.isEmpty
+          ? Column(
               children: [
+                Text(
+                  "No Transactions Added Yet!",
+                  style: Theme.of(context).textTheme.bodyText1,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
                 Container(
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.blueAccent, width: 2)),
-                  padding: EdgeInsets.all(10),
-                  child: Text(
-                    "\$${transactions[index].amount.toStringAsFixed(2)}",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Theme.of(context).primaryColor),
-                  ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(transactions[index].title,
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 17)),
-                    Text(
-                        DateFormat()
-                            .add_yMMMMd()
-                            .format(transactions[index].date),
-                        style: TextStyle(color: Colors.grey)),
-                  ],
-                ),
+                    height: 200,
+                    child: Image.asset(
+                      "./assets/images/waiting.png",
+                      fit: BoxFit.cover,
+                    ))
               ],
+            )
+          : ListView.builder(
+              itemBuilder: (ctx, index) {
+                return Card(
+                  child: Row(
+                    children: [
+                      Container(
+                        margin:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                        decoration: BoxDecoration(
+                            border:
+                                Border.all(color: Colors.blueAccent, width: 2)),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          "\$${transactions[index].amount.toStringAsFixed(2)}",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Theme.of(context).primaryColor),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(transactions[index].title,
+                              style: Theme.of(context).textTheme.bodyText1),
+                          Text(
+                              DateFormat()
+                                  .add_yMMMMd()
+                                  .format(transactions[index].date),
+                              style: TextStyle(color: Colors.grey)),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
+              itemCount: transactions.length,
             ),
-          );
-        },
-        itemCount: transactions.length,
-      ),
     );
   }
 }
